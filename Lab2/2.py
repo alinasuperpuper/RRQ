d94315c138f1af0d4e26c8d9031820e461ea6503
alinasuperpuper <146050080+alinasuperpuper@users.noreply.github.com>
2023-11-03 14:58:22 +0900

    Create 2.py

---
+++ Lab2/2.py
@@ -0,0 +1,10 @@
+def main():
+    a = input()
+    b = input()
+    if a and b == 'да' or a and b == 'нет':
+        print('ВЕРНО')
+    
+    else:
+        print('НЕВЕРНО')
+if __name__ == "__main__":
+    main()
