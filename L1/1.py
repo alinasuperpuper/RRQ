316afe7eaef4d9863e97a95ab1acec3ac545c708
alinasuperpuper <146050080+alinasuperpuper@users.noreply.github.com>
2023-11-03 14:47:18 +0900

    Create 1.py

---
+++ L1/1.py
@@ -0,0 +1,8 @@
+def main():
+    film = input()    # Считываем текст
+    kino = input()    # Считываем текст
+    time = input()    # Считываем текст
+    print(f'Билет на "{film}" в "{kino}" на {time} забронирован.')
+
+if __name__ == "__main__":
+    main()
