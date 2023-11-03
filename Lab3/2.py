e6cb5f1a7e68a69bf1d6901943267178eabba1b0
alinasuperpuper <146050080+alinasuperpuper@users.noreply.github.com>
2023-11-03 15:26:31 +0900

    Create 2.py

---
+++ Lab3/2.py
@@ -0,0 +1,18 @@
+def main():
+        b = float(input())
+        c = 0
+        if b > 36.6:
+            c = 1 
+        else:
+            print(0)
+    
+        while b < 36.6:
+            b = float(input())
+            if b < 0:
+                c += 1
+    
+        print(c)
+
+
+if __name__ == '__main__':
+    main()
