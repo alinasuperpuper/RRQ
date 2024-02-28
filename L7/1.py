407bd4191be8c3a82b19645fff200401623ffecd
alinasuperpuper <146050080+alinasuperpuper@users.noreply.github.com>
2024-02-28 13:39:13 +0900

    Create 1.py

---
+++ L7/1.py
@@ -0,0 +1,11 @@
+from q import factorial
+from w import fibonacci
+from e import count_vowels
+from r import is_prime
+from t import find_max
+
+print(factorial(5))
+print(fibonacci(5))
+print(count_vowels('sadsadsad'))
+print(is_prime(5))
+print(find_max([1, 2, 3]))
